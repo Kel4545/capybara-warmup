8b3e6a7c49b68bea942921e3331904d932c05a31
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-07-03 11:47:21 -0600

    Warmup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,31 @@ require "capybara/rspec"
 require "./rsvp_app"
 
 Capybara.app = RsvpApp
+
+feature "Homepage" do
+  scenario "should have RSVP link" do
+    visit "/"
+    click_link 'RSVP'
+    expect(page).to have_content("RSVP")
+  end
+end
+
+feature "RSVP Page" do
+  scenario "Fills in RSVP" do
+    visit "/rsvps/new"
+    fill_in 'First Name', :with => 'Kelly'
+    fill_in 'Email', :with => 'Kelly.Pederson@gmail.com'
+    fill_in 'Number of guests', :with => 'number of guests'
+    choose 'chicken'
+    click_link 'submit form'
+    expect(page).to have_content 'Thanks for RSVPing'
+  end
+end
+
+
+
+
+
+
+
+
